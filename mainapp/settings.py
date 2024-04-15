12558ce4842174a6ceb4aa6df7f8c0235a4c6b3a
Pankti Shah <pqshah@acg.aaa.com>
2024-04-15 19:08:38 -0400

    Updated Image size

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -131,7 +131,7 @@ OPENAI_API_KEY  = env("OPENAI_API_KEY",default=None)
 STREAMING = True
 
 IMAGE_GEN_MODEL = env("IMAGE_GEN_MODEL", default=None)
-SIZE="512x512"
+SIZE = env("IMAGE_SIZE", default="1024x1024")
 
 SERPER_API_KEY = env("SERPER_API_KEY",default=None)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
